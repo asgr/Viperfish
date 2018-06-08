0893718b3dbe64458c51e8e151bdf7781a194751
Aaron Robotham <aaron.robotham@uwa.edu.au>
2018-06-08 16:39:16 +0800

    Added write.SED

--- R/write.dataset.R
+++ R/write.dataset.R
@@ -48,3 +48,25 @@ write.custom.dataset = function(filename='temp.hdf5', # hd5 filename
     file.h5$close()
   }
 }
+
+write.SED=function(SED, filename='temp.hdf5', overwrite=FALSE, filters=c('FUV', 'NUV', 'u_SDSS', 'g_SDSS', 'r_SDSS', 'i_SDSS', 'Z_VISTA', 'Y_VISTA', 'J_VISTA', 'H_VISTA', 'K_VISTA', 'W1', 'W2', 'W3', 'W4', 'P100', 'P160', 'S250', 'S350', 'S500')){
+
+  Ncol=length(filters)
+  colrun=1:Ncol
+
+  write.custom.dataset(filename=filename, group="ab_nodust/bulge", object=testSED[,1+colrun], overwrite=overwrite)
+  write.custom.dataset(filename=filename, group="ab_nodust/disk", object=testSED[,1+colrun+Ncol], overwrite=overwrite)
+  write.custom.dataset(filename=filename, group="ab_nodust/total", object=testSED[,1+colrun+Ncol*2], overwrite=overwrite)
+
+  write.custom.dataset(filename=filename, group="ap_nodust/bulge", object=testSED[,1+colrun+Ncol*3], overwrite=overwrite)
+  write.custom.dataset(filename=filename, group="ap_nodust/disk", object=testSED[,1+colrun+Ncol*4], overwrite=overwrite)
+  write.custom.dataset(filename=filename, group="ap_nodust/total", object=testSED[,1+colrun+Ncol*5], overwrite=overwrite)
+
+  write.custom.dataset(filename=filename, group="ab_dust/bulge", object=testSED[,1+colrun+Ncol*6], overwrite=overwrite)
+  write.custom.dataset(filename=filename, group="ab_dust/disk", object=testSED[,1+colrun+Ncol*7], overwrite=overwrite)
+  write.custom.dataset(filename=filename, group="ab_dust/total", object=testSED[,1+colrun+Ncol*8], overwrite=overwrite)
+
+  write.custom.dataset(filename=filename, group="ap_dust/bulge", object=testSED[,1+colrun+Ncol*9], overwrite=overwrite)
+  write.custom.dataset(filename=filename, group="ap_dust/disk", object=testSED[,1+colrun+Ncol*10], overwrite=overwrite)
+  write.custom.dataset(filename=filename, group="ap_dust/total", object=testSED[,1+colrun+Ncol*11], overwrite=overwrite)
+}
