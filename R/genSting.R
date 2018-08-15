64d80cc058b9bd1d036344e04866342db075f4ea
Aaron Robotham <aaron.robotham@uwa.edu.au>
2018-08-15 15:48:17 +0800

    maybe now...

--- R/genSting.R
+++ R/genSting.R
@@ -103,13 +103,8 @@ genSting=function(file_sting='mocksurvey.hdf5', path_shark='.', h=0.678, cores=4
     Zdisk_subsnap=SFHsing_subsnap$Zdisk/h
 
     tempout=foreach(j=1:length(select), .combine='rbind')%do%{
-      tempSED=tryCatch(unlist(genSED(SFRbulge_d=SFRbulge_d_subsnap[j,], SFRbulge_m=SFRbulge_m_subsnap[j,], SFRdisk=SFRdisk_subsnap[j,], redshift=zobs[j], time=time[1:dim(SFRdisk_subsnap)[2]]-cosdistTravelTime(zcos[j], ref='planck')*1e9, speclib=BC03lr, Zbulge_d=Zbulge_d_subsnap[j,], Zbulge_m=Zbulge_m_subsnap[j,], Zdisk=Zdisk_subsnap[j,], filtout=filtout, Dale=Dale_Msol, sparse=sparse, tau_birth=tau_birth, tau_screen=tau_screen, intSFR = intSFR)), error = function(x) 'error')
+      tempSED=tryCatch(unlist(genSED(SFRbulge_d=SFRbulge_d_subsnap[j,], SFRbulge_m=SFRbulge_m_subsnap[j,], SFRdisk=SFRdisk_subsnap[j,], redshift=zobs[j], time=time[1:dim(SFRdisk_subsnap)[2]]-cosdistTravelTime(zcos[j], ref='planck')*1e9, speclib=BC03lr, Zbulge_d=Zbulge_d_subsnap[j,], Zbulge_m=Zbulge_m_subsnap[j,], Zdisk=Zdisk_subsnap[j,], filtout=filtout, Dale=Dale_Msol, sparse=sparse, tau_birth=tau_birth, tau_screen=tau_screen, intSFR = intSFR)), error = function(e) NULL)
       print(tempSED)
-      if(tempSED[1]=='error'){
-        tempSED=NULL
-      }else{
-        tempSED=c(id_galaxy[j], tempSED)
-      }
       tempSED
     }
     as.data.table(rbind(tempout))
