5dce6dd711244e477b44b84d02fc4b9914ff965b
Aaron Robotham <aaron.robotham@uwa.edu.au>
2018-09-12 21:23:07 +0800

    configr -> ini

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,5 @@
-get_shark_path = function(config_file)
-{
+get_shark_path = function(config_file){
   assertAccess(config_file, access='r')
-  cfg = configr::read.config(config_file)
+  cfg = read.ini(config_file)
   path = paste(cfg$execution$output_directory, cfg$simulation$sim_name, cfg$execution$name_model, sep='/')
 }
