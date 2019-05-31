c7fca8ae87bda52e50b293e16955b2d917e5ac39
Claudia Lagos <claudia.lagos@icrar.org>
2019-05-31 14:44:10 +0800

    Added option to read extinction tau values
    This can be directly read from hdf5 files being written for Shark galaxies.
    The assumption though is that the bulge is a single dust phase, while the disk is
    a two-phase medium.

--- R/genShark.R
+++ R/genShark.R
@@ -1,4 +1,4 @@
-genShark=function(path_shark='.', snapshot=NULL, subvolume=NULL, redshift="get", h='get', cores=4, id_galaxy_sam='all', filters=c('FUV_GALEX', 'NUV_GALEX', 'u_SDSS', 'g_SDSS', 'r_SDSS', 'i_SDSS', 'Z_VISTA', 'Y_VISTA', 'J_VISTA', 'H_VISTA', 'K_VISTA', 'W1_WISE', 'W2_WISE', 'W3_WISE', 'W4_WISE', 'P100_Herschel', 'P160_Herschel', 'S250_Herschel', 'S350_Herschel', 'S500_Herschel'), tau_birth=1.5, tau_screen=0.5, sparse=5, final_file_output='Shark-SED.csv', intSFR=TRUE, verbose=TRUE, write_final_file=FALSE){
+genShark=function(path_shark='.', snapshot=NULL, subvolume=NULL, redshift="get", h='get', cores=4, id_galaxy_sam='all', filters=c('FUV_GALEX', 'NUV_GALEX', 'u_SDSS', 'g_SDSS', 'r_SDSS', 'i_SDSS', 'Z_VISTA', 'Y_VISTA', 'J_VISTA', 'H_VISTA', 'K_VISTA', 'W1_WISE', 'W2_WISE', 'W3_WISE', 'W4_WISE', 'P100_Herschel', 'P160_Herschel', 'S250_Herschel', 'S350_Herschel', 'S500_Herschel'), tau_birth=1.5, tau_screen=0.5, read_extinct=FALSE, sparse=5, final_file_output='Shark-SED.csv', intSFR=TRUE, verbose=TRUE, write_final_file=FALSE){
 
   timestart=proc.time()[3]
 
@@ -40,6 +40,11 @@ genShark=function(path_shark='.', snapshot=NULL, subvolume=NULL, redshift="get",
   assertAccess(sfh_fname, access='r')
   Shark_SFH=h5file(sfh_fname, mode='r')
 
+  
+  extinct_fname = paste(path_shark, snapshot, subvolume, 'extinction.hdf5',sep='/')
+  assertAccess(extinct_fname, access='r')
+  Shark_Extinct=h5file(extinct_fname, mode='r')
+
   # Read things in from the Shark HDF5 file:
 
   time=Shark_SFH[['lbt_mean']][]*1e9
@@ -69,6 +74,28 @@ genShark=function(path_shark='.', snapshot=NULL, subvolume=NULL, redshift="get",
   Zbulge_m=Shark_SFH[['bulges_mergers/metallicity_histories']][,select,drop=FALSE]
   Zdisk=Shark_SFH[['disks/metallicity_histories']][,select,drop=FALSE]
 
+  #define tau in extinction laws
+  tau_dust = matrix(ncol = 3, nrow = length(SFRbulge_d)) #this is ordered as bulge (disk-ins), bulge (mergers), disks
+  tau_clump = matrix(ncol = 3, nrow = length(SFRbulge_d)) #this is ordered as bulge (disk-ins), bulge (mergers), disks
+ 
+  if(read_extinct){
+     #read in disks
+     tau_dust[,3] = Shark_Extinct[['galaxies/tau_diff_disk']][,select,drop=FALSE]
+     tau_clump[,3] = Shark_Extinct[['galaxies/tau_clump_disk']][,select,drop=FALSE]
+
+     tau_dust[,1] = Shark_Extinct[['galaxies/tau_diff_bulge']][,select,drop=FALSE]
+     #bulges are a single phase
+     tau_clump[,1] = tau_dust[,1]
+
+     #assume the same for bulges regardless of origin of star formation
+     tau_dust[,2] = tau_dust[,1]
+     tau_clump[,2] = tau_clump[,1]
+  }
+  else{
+    tau_dust[,] = tau_screen
+    tau_clump[,] = tau_birth
+  }
+
   if(length(redshift)==1){
     redshift=rep(redshift, length(select))
   }
@@ -91,7 +118,7 @@ genShark=function(path_shark='.', snapshot=NULL, subvolume=NULL, redshift="get",
   # Here we divide by h since the simulations output SFR in their native Msun/yr/h units.
 
   outSED=foreach(i=1:iterations, .combine='rbind', .options.snow = if(verbose){opts})%dopar%{
-  unlist(genSED(SFRbulge_d=SFRbulge_d[,i]/h, SFRbulge_m=SFRbulge_m[,i]/h, SFRdisk=SFRdisk[,i]/h, redshift=redshift[i], time=time, speclib=BC03lr, Zbulge_d=Zbulge_d[,i], Zbulge_m=Zbulge_m[,i], Zdisk=Zdisk[,i], filtout=filtout, Dale=Dale_Msol, tau_birth=tau_birth, tau_screen=tau_screen, sparse=sparse, intSFR=intSFR))
+  unlist(genSED(SFRbulge_d=SFRbulge_d[,i]/h, SFRbulge_m=SFRbulge_m[,i]/h, SFRdisk=SFRdisk[,i]/h, redshift=redshift[i], time=time, speclib=BC03lr, Zbulge_d=Zbulge_d[,i], Zbulge_m=Zbulge_m[,i], Zdisk=Zdisk[,i], filtout=filtout, Dale=Dale_Msol, tau_birth=tau_clump[,i], tau_screen=tau_dust[,i], sparse=sparse, intSFR=intSFR))
   }
 
   stopCluster(cl)
